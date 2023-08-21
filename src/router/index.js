ace76fd74ef89094bbcb72bcdd49da8a63ac8d16
beckynayere <rebecca.nayere@gmail.com>
2023-08-21 17:22:31 +0300

    add export default

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from "@/components/HelloWorld"
+import Register from "@/components/Register  "
+// import router from './router'
+
 
 Vue.use(Router)
+
+// const router = new Router({
+export default new Router({
 routes: [
     {
         path:'/',
         name:'HelloWorld',
         component: HelloWorld
     },
+    {
+        path: '/register',
+        name:'Register',
+        component:Register
+    },
 
 ]
 })
+
